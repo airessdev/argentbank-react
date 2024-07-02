e54b84829ca4e4cde3199cb6f0798578aa471730
renaud subrini <145485715+airessdev@users.noreply.github.com>
2024-07-02 12:44:45 +0200

    Update Home.jsx
    correct indent

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,21 +4,12 @@ import Header from '../../components/Header/Header';
 import Middle from '../../components/Middle/Middle';
 import Footer from '../../components/Footer/Footer';
 
-
-
-
-
-
-
 const Home = () => {
     return (
         <div className="home">
             <Header />
             <Middle />
-            
             <Footer />
-
-
         </div>
     );
 };
